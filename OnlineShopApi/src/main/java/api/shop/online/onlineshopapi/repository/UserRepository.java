5e0078dd1f8f26f73b430eae6983efc9ec87e749
zimch <gregoryshanin.job@gmail.com>
2023-03-27 17:09:51 +0300

    add-user-api

--- OnlineShopApi/src/main/java/api/shop/online/onlineshopapi/repository/UserRepository.java
+++ OnlineShopApi/src/main/java/api/shop/online/onlineshopapi/repository/UserRepository.java
@@ -4,10 +4,15 @@ import api.shop.online.onlineshopapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.login = ?1")
+    User findByUsername(String username);
+
     Optional<User> findByLogin(String login);
 }
--- OnlineShopApi/src/main/java/api/shop/online/onlineshopapi/rest/UserRestController.java
+++ OnlineShopApi/src/main/java/api/shop/online/onlineshopapi/rest/UserRestController.java
@@ -2,11 +2,15 @@ package api.shop.online.onlineshopapi.rest;
 
 import api.shop.online.onlineshopapi.model.Organization;
 import api.shop.online.onlineshopapi.model.User;
+import api.shop.online.onlineshopapi.repository.UserRepository;
 import api.shop.online.onlineshopapi.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -16,9 +20,14 @@ public class UserRestController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping(value = "/organizations", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Organization> getAllOrganizations() {
-        return new ResponseEntity<>(new Organization(), HttpStatus.OK);
+    public ResponseEntity<Organization> getOrganization(HttpServletRequest httpServletRequest) {
+        Principal userPrincipal = httpServletRequest.getUserPrincipal();
+        User user = userRepository.findByUsername(userPrincipal.getName());
+        return new ResponseEntity<>(user.getOrganization(), HttpStatus.OK);
     }
 
 }
